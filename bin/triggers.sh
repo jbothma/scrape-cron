219793e2ae12e87e47e398c466814a3345882a99
JD Bothma <jbothma@gmail.com>
2017-10-20 12:53:48 +0200

    Cleaner output for easier debugging

--- bin/triggers.sh
+++ bin/triggers.sh
@@ -1,6 +1,6 @@
-set -eo pipefail
+set -xeo pipefail
 
 for SPIDER in $SCRAPINGHUB_SPIDERS
 do
-    curl --fail --http1.0 -v -u $SCRAPINGHUB_API_KEY: https://app.scrapinghub.com/api/run.json -d project=79283 -d spider=$SPIDER
+    curl --fail --http1.0 -i -u $SCRAPINGHUB_API_KEY: https://app.scrapinghub.com/api/run.json -d project=79283 -d spider=$SPIDER
 done
