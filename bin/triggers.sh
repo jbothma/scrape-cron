db3a4d04d573f3aea577b6105a944196d66e8cca
JD Bothma <jbothma@gmail.com>
2020-06-21 21:11:44 +0200

    replace key and spiders

--- bin/triggers.sh
+++ bin/triggers.sh
@@ -1,5 +1,4 @@
 set -xeo pipefail
 
-curl --fail --http1.0 -i -u $SCRAPINGHUB_API_KEY: https://app.scrapinghub.com/api/run.json -d project=79283 -d spider=$SCRAPINGHUB_SPIDER1
-curl --fail --http1.0 -i -u $SCRAPINGHUB_API_KEY: https://app.scrapinghub.com/api/run.json -d project=79283 -d spider=$SCRAPINGHUB_SPIDER2
-curl --fail --http1.0 -i -u $SCRAPINGHUB_API_KEY: https://app.scrapinghub.com/api/run.json -d project=79283 -d spider=$SCRAPINGHUB_SPIDER3
+curl --fail --http1.0 -i -u $SCRAPINGHUB_API_KEY: https://app.scrapinghub.com/api/run.json -d project=383734 -d spider=advertised-tenders
+curl --fail --http1.0 -i -u $SCRAPINGHUB_API_KEY: https://app.scrapinghub.com/api/run.json -d project=383734 -d spider=awarded-tenders
